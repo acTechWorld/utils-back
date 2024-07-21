f11fb85cbbe6ffc21f838b14c8134aad9842222d
vue-landing <vuelandingcontact@gmail.com>
2024-07-21 21:29:58 +0200

    small updates

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: '109.234.166.190', // e.g., 'localhost' or a remote IP address
+  host: 'localhost', // e.g., 'localhost' or a remote IP address
   user: 'fule0038_utils-backend', // your database username
   password: 'utils-backend2024+', // your database password
   database: 'fule0038_utils-backend-bdd', // your database name
