e6c1163952d751a8f134e2b636bc22f6c2dd0381
oncekiller <canard.antoine@gmail.com>
2024-07-21 17:35:42 +0200

    add github service

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // index.js
 
 const express = require('express');
-const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-const mappingSourceEmail= {
-    vueLanding: 'vuelandingcontact@gmail.com'
-}
+const emailService = require('./services/emailService');
+const githubService = require('./services/githubService');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,41 +16,9 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
+// Routes
+app.use('/api', githubService);
 
-// POST endpoint to handle form submissions
-app.post('/send-email', (req, res) => {
-    const { source, firstname, lastname, email, companyName, phoneNumber, message } = req.body;
-
-    // Create a transporter object using Gmail SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
-
-    // Setup email data
-    let mailOptions = {
-        from: email,
-        to: mappingSourceEmail[source], // Your receiving email address
-        subject: `New contact form submission from ${firstname} ${lastname}`,
-        text: 
-        `Name: ${firstname} ${lastname}
-Email: ${email}
-Company Name: ${companyName}
-Phone Number: ${phoneNumber}
-Message: ${message}`,
-    };
-
-    // Send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return res.status(500).send(error.toString());
-        }
-        res.status(200).send('Message sent successfully!');
-    });
-});
 
 // Start the server
 app.listen(port, () => {
---
+++ services/emailService.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const express = require('express');
+
+const router = express.Router();
+const mappingSourceEmail= {
+    vueLanding: 'vuelandingcontact@gmail.com'
+}
+
+
+// POST endpoint to handle form submissions
+router.post('/email/send-email', (req, res) => {
+    const { source, firstname, lastname, email, companyName, phoneNumber, message } = req.body;
+
+    // Create a transporter object using Gmail SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    // Setup email data
+    let mailOptions = {
+        from: email,
+        to: mappingSourceEmail[source], // Your receiving email address
+        subject: `New contact form submission from ${firstname} ${lastname}`,
+        text: 
+        `Name: ${firstname} ${lastname}
+Email: ${email}
+Company Name: ${companyName}
+Phone Number: ${phoneNumber}
+Message: ${message}`,
+    };
+
+    // Send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Message sent successfully!');
+    });
+});
+
+module.exports = router;
