e4d4c1966c1b87710872c55cb495b19b4c65bd80
vue-landing <vuelandingcontact@gmail.com>
2024-08-03 21:45:05 +0200

    add IP whitelisting for local only

--- index.js
+++ index.js
@@ -16,35 +16,54 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 
+// Allowed origins
+const whiteListedOrigins = [
+    'http://vuelanding.com'
+];
 
+//IP security
+const allowedIPs = [
+    '86.195.103.2', // My IP
+];
 
-// Configure CORS to only allow requests from your frontend domain
+// Configure CORS to allow specific origins
 const corsOptions = {
-    origin: 'http://vuelanding.com', // Replace with your frontend's domain
+    origin: function (origin, callback) {
+  
+      // Allow all localhost origins dynamically
+      if (!origin || whiteListedOrigins.includes(origin) || /^http:\/\/localhost(:[0-9]+)?$/.test(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     optionsSuccessStatus: 200,
-    credentials: true // This allows cookies to be sent
+    credentials: true // Allows cookies to be sent
 };
 
 app.use(cors(corsOptions));
 
-//IP security
-const allowedIPs = [
-    '86.195.103.2', // Replace with actual allowed IP addresses
-];
-
 // Middleware to check for allowed IP addresses
 const ipWhitelist = (req, res, next) => {
-    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const origin = req.headers.origin;
     
+    // If origin is in whitelisted origin don't check IP (for frontends access) skip IP check
+    if (whiteListedOrigins.includes(origin)) {
+      return next();
+    }
+  
+    // Get client IP address
+    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
     // Remove any port information from the IP address
     const cleanIP = clientIP.split(':')[0];
-    
+  
     if (allowedIPs.includes(cleanIP)) {
       next();
     } else {
       res.status(403).send('Access forbidden');
     }
-};
+  };
 // Apply IP whitelist middleware
 app.use(ipWhitelist);
   
